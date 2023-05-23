f4aa1fd3822113f055990f4c7cc1c4264663746f
Pierre Kisters <pierrekisters@gmail.com>
2023-05-23 11:58:15 +0200

    ENTRYPOINT

--- swarm-dind.sh
+++ swarm-dind.sh
@@ -9,20 +9,20 @@ getCurrentContainerId() {
 }
 
 getContainerEnv() {
-  local -n vars="$1"
+  local -n _vars="$1"
   local containerId="$2"
   while IFS= read -r var; do
-    vars+=("$(echo "$var" | gojq -r)")
+    _vars+=("$(echo "$var" | gojq -r)")
   done < <(
     docker container inspect --format '{{range .Config.Env}}{{println (json .)}}{{end}}' "$containerId"
   )
 }
 
 getContainerLabels() {
-  local -n labels="$1"
+  local -n _labels="$1"
   local containerId="$2"
   while IFS= read -r label; do
-    labels+=("$(echo "$label" | gojq -r)")
+    _labels+=("$(echo "$label" | gojq -r)")
   done < <(
     docker container inspect --format '{{range $k,$v:=.Config.Labels}}{{println (json (printf "%s=%s" $k $v))}}{{end}}' "$containerId"
   )
